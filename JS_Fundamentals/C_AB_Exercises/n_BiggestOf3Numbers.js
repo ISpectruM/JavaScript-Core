954afd6b75c6aca2bb09ad578418d4eb6bd3032c
ISpectruM <ispectrumcode@gmail.com>
2017-05-27 10:43:48 +0300

    Data,Expressions,Statements exercises - problem 14 solved.

---
+++ JS_Fundamentals/C_AB_Exercises/n_BiggestOf3Numbers.js
@@ -0,0 +1,7 @@
+function biggestNumber(numbers) {
+    let biggestNum = Math.max(numbers[0],numbers[1],numbers[2]);
+
+    console.log(biggestNum);
+}
+
+// (numbers) => Math.max(numbers[0],numbers[1],numbers[2]);
