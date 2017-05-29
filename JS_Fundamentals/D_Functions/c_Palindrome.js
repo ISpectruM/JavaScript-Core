604f2b7ff9dcfa77cb3a32cebe0a3fdd82c86140
ISpectruM <ispectrumcode@gmail.com>
2017-05-29 17:10:38 +0300

    Functions Lab - problem 3 solved.

---
+++ JS_Fundamentals/D_Functions/c_Palindrome.js
@@ -0,0 +1,11 @@
+function isPalindrome(string) {
+    for (let i=0; i<string.length/2; i++){
+        if (string[i] !== string[string.length-i-1]){
+            return false;
+        }
+    }
+
+    return true;
+}
+
+console.log(isPalindrome('haha'));
