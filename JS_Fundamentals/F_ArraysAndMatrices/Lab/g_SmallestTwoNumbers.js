9c54993d33a45031162dedc25a5acf4ccb13c12b
ISpectruM <ispectrumcode@gmail.com>
2017-06-02 17:01:46 +0300

    Arrays and Matrices Lab - problem 7 solved.

---
+++ JS_Fundamentals/F_ArraysAndMatrices/Lab/g_SmallestTwoNumbers.js
@@ -0,0 +1,8 @@
+function getSmallestTwoNumbers(arr) {
+    let result = arr.sort((a,b) => a-b)
+        .slice(0,2)
+        .join(" ");
+    console.log(result);
+}
+
+getSmallestTwoNumbers([3, 0, 10, 4, 7, 3]);
