894a816ab6f9443bec9c8d20bc163f312eedbec7
ISpectruM <ispectrumcode@gmail.com>
2017-06-09 14:37:30 +0300

    String and Regex Exercises - problem 7 solved.

---
+++ JS_Fundamentals/G_StringsAndRegExp/Exercises/g_FindVariableNames.js
@@ -0,0 +1,14 @@
+function findNames(string) {
+    let regex = /\b(_)([A-Za-z0-9]+)\b/g;
+    let result = [];
+    let match = regex.exec(string);
+    while (match){
+        result.push(match[2]);
+        match = regex.exec(string);
+    }
+    console.log(result.join(','));
+}
+
+findNames('The _id and _age variables are both integers.');
+findNames('Calculate the _area of the _perfectRectangle object.');
+findNames('__invalidVariable _evenMoreInvalidVariable_ _validVariable');
