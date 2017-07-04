e7df65b1852a936edd6788e8f436b2624ec1398d
ISpectruM <ispectrumcode@gmail.com>
2017-07-04 14:42:23 +0300

    Advanced Functions Lab - Problem 5 added.

---
+++ JS Advanced/E_AdvancedFunctions/Lab/e_next-article.js
@@ -0,0 +1,12 @@
+function getArticleGenerator(articles) {
+    let container = $('#content');
+
+    return function () {
+        if (articles.length >0) {
+            let article = $('<article>');
+            article.append($(`<p>${articles.shift()}</p>`));
+            container.append(article);
+        }
+    }
+}
+
