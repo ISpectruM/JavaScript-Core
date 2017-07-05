959d5a278654cfadfed6e0f4e372d6563a927435
ISpectruM <ispectrumcode@gmail.com>
2017-07-05 10:41:25 +0300

    AF Exercises - Problem 5 solved.

---
+++ JS Advanced/E_AdvancedFunctions/Exercises/e_VectorMath.js
@@ -0,0 +1,17 @@
+let solution = (function () {
+
+    return {
+        add:(a,b)=> [a[0]+b[0],a[1]+b[1]],
+        multiply:(a,b)=> [a[0]*b,a[1]*b],
+        length: (e)=> Math.sqrt(e[0]*e[0] + e[1]*e[1]),
+        dot: (a,b) => (a[0]*b[0] + a[1]*b[1]),
+        cross:(a,b) => (a[0]*b[1] - a[1]*b[0])
+    };
+})();
+
+console.log(solution.add([1, 1], [1, 0]));
+console.log(solution.multiply([3.5, -2], 2));
+console.log(solution.length([3, -4]));
+console.log(solution.dot([2, 3], [2, -1]));
+console.log(solution.cross([3, 7], [1, 0]));
+
