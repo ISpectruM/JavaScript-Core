39c25fbd1a1914c214877633e36fac76074c0567
ISpectruM <ispectrumcode@gmail.com>
2017-07-07 14:25:31 +0300

    Object composition exercises - Problem 2 solved.

---
+++ JS Advanced/F_ObjectComposition/Exercises/b_ConstructionCrew.js
@@ -0,0 +1,20 @@
+function modifyWorker(worker) {
+
+    if (worker.handsShaking === true){
+        let required = worker.weight * 0.1 * worker.experience;
+        worker.bloodAlcoholLevel += required;
+        worker.handsShaking = false;
+    }
+    return worker;
+}
+
+let worker = { weight: 95,
+    experience: 3,
+    bloodAlcoholLevel: 0,
+    handsShaking: false }
+
+;
+
+modifyWorker(worker);
+
+
