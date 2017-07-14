b86a42a04924b4bbd4bb0570028e97c92c0a8cb7
ISpectruM <ispectrumcode@gmail.com>
2017-07-14 15:29:37 +0300

    Classes Exercises - Problem 6 solved.

---
+++ B_JS Advanced/H_JS_Classes/Exercises/f_SortedList.js
@@ -0,0 +1,23 @@
+class SortedList{
+    constructor(){
+        this.internalArray = [];
+        this.size = 0;
+    }
+
+    add(element) {
+        this.internalArray.push(element);
+        this.size++;
+        this.internalArray.sort((a, b) => a-b);
+    }
+    remove(index){
+        if (index >= 0 && index < this.internalArray.length){
+            this.internalArray = this.internalArray.filter((e, i) => i !== index);
+            this.size--;
+        }
+    }
+    get(index) {
+        if (index >= 0 && index < this.internalArray.length){
+            return this.internalArray[index];
+        }
+    }
+}
