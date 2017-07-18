e02240e024d5aa10f0042b2e720f1695c3ee77e3
ISpectruM <ispectrumcode@gmail.com>
2017-07-18 15:39:52 +0300

    Modules Lab- Problem 2 solved.

---
+++ B_JS Advanced/J_Modules/b_SortMap/helper-functions.js
@@ -0,0 +1,10 @@
+function mapSort(map, sortFn){
+
+    if (sortFn !== undefined){
+        return new Map([...map.entries()].sort(sortFn));
+    }else {
+        return new Map([...map.entries()].sort((a,b)=> a[0]>b[0]));
+    }
+}
+
+module.exports = mapSort;
