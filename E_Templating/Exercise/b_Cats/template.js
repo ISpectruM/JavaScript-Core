48383bd2f49db3bcb338ba73673d11bc431dc636
ISpectruM <ispectrumcode@gmail.com>
2017-08-12 14:18:18 +0300

    JS Templating exercises - problem 2 solved.

--- E_Templating/Exercise/b_Cats/template.js
+++ E_Templating/Exercise/b_Cats/template.js
@@ -1,8 +1,41 @@
 $(() => {
-    renderCatTemplate();
+    loadTemplates();
 
-    function renderCatTemplate() {
-        // TODO: Render cat template and attach events
+    const container = $('#allCats');
+    const context = {
+        cats:window.cats
+    };
+    const source = document.getElementById('cat-template').innerHTML;
+
+    async function loadTemplates(){
+        let partialTemplate = await $.get('./templates/cats.html');
+
+        Handlebars.registerPartial('cat',partialTemplate);
+        let template = Handlebars.compile(source);
+
+        renderCatTemplate(template);
     }
 
+    function renderCatTemplate(template) {
+        container.html(template(context));
+        attachBtnEvents();
+    }
+
+    function attachBtnEvents() {
+        let btn = $('.btn');
+        btn.click((e)=>{
+            let id = $(e.target).next().attr('id');
+            showDetails(id);
+        });
+        function showDetails(id) {
+            let info = $(`#${id}`).toggle();
+            if (info.css('display') === 'block'){
+                // info.css('display','block');
+                info.prev().text('Hide status code')
+            } else {
+                // info.css('display','none');
+                info.prev().text('Show status code')
+            }
+        }
+    }
 })
